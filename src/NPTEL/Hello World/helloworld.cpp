3a190c0849eb09fab42f55119b511594b2655e26
alizaib <alizaib@gmail.com>
2024-09-19 16:37:23 +0500

    hello world gcc commands added

--- src/NPTEL/Hello World/helloworld.cpp
+++ src/NPTEL/Hello World/helloworld.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
-int& fnRetByVal(int& x) { 
-    cout<<"x = "<< x <<" &x= "<< &x <<endl;
-    return x;
-}
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};    
-    cout << endl;
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-
-    int a = 10;
-    cout<<"a = "<< a <<" &a= "<< &a <<endl;
-
-    const int& b = fnRetByVal(a);
-    cout<<"b = "<< b <<" &b= "<< &b <<endl;   
+    printf("Hello World!!");
 }
+
+/*
+    To compile with gcc
+    gcc -c helloworld.cpp    // will produce helloworld.o
+    gcc helloworld.o -o helloworld // will produce the exe
+    or if you wanna do it one step
+    gcc helloworld.cpp -o helloworld //will produce the exe
+
+    to run the exe type
+    ./helloworld
+
+    gcc -S helloworld.cpp   //with capital S will produce the assembly language version of your code
+*/
---
+++ src/NPTEL/Hello World/helloworld.s
@@ -0,0 +1,75 @@
+	.file	"helloworld.cpp"
+	.text
+	.section	.text$_Z6printfPKcz,"x"
+	.linkonce discard
+	.globl	_Z6printfPKcz
+	.def	_Z6printfPKcz;	.scl	2;	.type	32;	.endef
+	.seh_proc	_Z6printfPKcz
+_Z6printfPKcz:
+.LFB304:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	pushq	%rbx
+	.seh_pushreg	%rbx
+	subq	$56, %rsp
+	.seh_stackalloc	56
+	leaq	48(%rsp), %rbp
+	.seh_setframe	%rbp, 48
+	.seh_endprologue
+	movq	%rcx, 32(%rbp)
+	movq	%rdx, 40(%rbp)
+	movq	%r8, 48(%rbp)
+	movq	%r9, 56(%rbp)
+	leaq	40(%rbp), %rax
+	movq	%rax, -16(%rbp)
+	movq	-16(%rbp), %rbx
+	movl	$1, %ecx
+	movq	__imp___acrt_iob_func(%rip), %rax
+	call	*%rax
+	movq	%rax, %rcx
+	movq	32(%rbp), %rax
+	movq	%rbx, %r8
+	movq	%rax, %rdx
+	call	__mingw_vfprintf
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addq	$56, %rsp
+	popq	%rbx
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello World!!\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB2239:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	_Z6printfPKcz
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE:
+	.byte	1
+_ZNSt8__detail30__integer_to_chars_is_unsignedImEE:
+	.byte	1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE:
+	.byte	1
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev3, Built by MSYS2 project) 14.1.0"
+	.def	__mingw_vfprintf;	.scl	2;	.type	32;	.endef
